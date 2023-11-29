71c8a23177c2de46355bc29fd6e8c0a9159ec8da
Jason Merrill <jasonrmerrill@icloud.com>
2023-11-29 09:54:41 -0500

    added some error handling

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -3,12 +3,16 @@ const appConfig = require('../appConfig.js'),
     transporter = nodemailer.createTransport(appConfig.mailConfig);
 
 exports.send = function(req, res) {
-    let mailOptions = {
-        from: appConfig.mailConfig.auth.user,
-        to: appConfig.mailConfig.auth.user,
-        subject: `gutbomb.net contact form message from ${req.body.emailAddress}`,
-        text: req.body.message
-    };
-    transporter.sendMail(mailOptions, (e) => {console.error(e)}, (r) => {console.log(r)});
-    return res.json({'status': 'message sent successfully'})
+    if (req.body.emailAddress && req.body.message) {
+        let mailOptions = {
+            from: appConfig.mailConfig.auth.user,
+            to: appConfig.mailConfig.auth.user,
+            subject: `gutbomb.net contact form message from ${req.body.emailAddress}`,
+            text: req.body.message
+        };
+        transporter.sendMail(mailOptions, (e) => {console.error(e)}, (r) => {console.log(r)});
+        return res.json({'status': 'message sent successfully'})
+    } else {
+        return res.json({'status': 'no message sent'});
+    }
 }
